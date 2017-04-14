7cb6402fd0ea883a68d1d765e5ae6bcdd9fefe9e
Tran Van Dong <tranvandong@trans-macbook-pro.local>
2017-04-14 22:17:19 +0700

    Initial Commit

---
+++ Restaurant Management/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Restaurant Management
+//
+//  Created by Tran Van Dong on 4/14/17.
+//  Copyright © 2017 Tran Van Dong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
