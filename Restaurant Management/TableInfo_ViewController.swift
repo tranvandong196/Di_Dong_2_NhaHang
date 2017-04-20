9b2413df2231821e7b00dd649df5cc4c011487a8
Nguyễn Đình Sơn <001.IceTea@gmail.com>
2017-04-20 17:32:17 +0700

    Hiện DS món đã đặt

--- Restaurant Management/TableInfo_ViewController.swift
+++ Restaurant Management/TableInfo_ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
-    // MARK: *** Global Variable
+// MARK: *** Global Variable
 var indexSelected_foods = 0
+var list_BookedFoods = [Food]()
 
 class TableInfo_ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
     var isAdded2 = false
@@ -24,7 +25,7 @@ class TableInfo_ViewController: UIViewController,UITableViewDataSource,UITableVi
     @IBOutlet weak var PositionTable_Button: UIButton!
     @IBOutlet weak var saveChanged_Button: UIBarButtonItem!
     @IBOutlet weak var otherInfo: UITextView!
-      @IBOutlet weak var payTable_Button: UIButton!
+    @IBOutlet weak var payTable_Button: UIButton!
     // MARK: *** Display view
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +44,74 @@ class TableInfo_ViewController: UIViewController,UITableViewDataSource,UITableVi
         
         
         Areas = GetAreasFromSQLite(query: "SElECT * FROM KhuVuc WHERE MaKV = \(Tables[indexSelected_tables].MaKV!)")
+        
+        
+        if(Tables[indexSelected_tables].MaHD != nil){
+            let str = "SELECT * FROM MonAn NATURAL JOIN (SELECT * FROM ChiTietHoaDon WHERE MaHD = " + "\(Tables[indexSelected_tables].MaHD!)" + ")"
+            
+            //list_BookedFoods = GetFoodsFromSQLite(query:  str)
+            
+            list_BookedFoods.removeAll()
+            
+            //sqlite
+            database = Connect_DB_SQLite(dbName: DBName, type: DBType)
+            
+            //Lay data
+            let statement:OpaquePointer = Select(query: str, database: database!)
+            
+            // Do du lieu vao mang
+            while sqlite3_step(statement) == SQLITE_ROW {
+                // Do ra tung cot tuong ung voi no
+                let food = Food()
+                
+                if(sqlite3_column_text(statement, 0) != nil)
+                {
+                    food.MaMon = Int(sqlite3_column_int(statement, 0))
+                    if(sqlite3_column_text(statement, 1) != nil)
+                    {
+                        food.TenMon = String(cString: sqlite3_column_text(statement, 1))
+                    }
+                    if(sqlite3_column_text(statement, 2) != nil)
+                    {
+                        food.Gia = Double(sqlite3_column_double(statement, 2))
+                    }
+                    if(sqlite3_column_text(statement, 3) != nil)
+                    {
+                        food.HinhAnh = String(cString: sqlite3_column_text(statement, 3))
+                        
+                    }
+                    if(sqlite3_column_text(statement, 3) != nil)
+                    {
+                        food.HinhAnh = String(cString: sqlite3_column_text(statement, 3))
+                    }
+                    if(sqlite3_column_text(statement, 4) != nil)
+                    {
+                        food.MoTa = String(cString: sqlite3_column_text(statement, 4))
+                    }
+                    if(sqlite3_column_text(statement, 5) != nil)
+                    {
+                        food.Loai = Int(sqlite3_column_int(statement, 5))
+                    }
+                    if(sqlite3_column_text(statement, 6) != nil)
+                    {
+                        food.Icon = String(cString: sqlite3_column_text(statement, 6))
+                    }
+                }
+                
+                list_BookedFoods.append(food)
+                
+                
+                //let rowData = sqlite3_column_text(statement, 1)
+                // Neu cot nao co dau tieng viet thi can phai lam them buoc nay
+                //let fieldValue = String(cString: rowData!)
+                // Them Vao mang da co
+                //mang.append(fieldValue!)
+            }
+            sqlite3_finalize(statement)
+            sqlite3_close(database)
+            
+            
+        }
         
         foods_TableView.reloadData()
         setup_displayBegin()
@@ -67,11 +136,11 @@ class TableInfo_ViewController: UIViewController,UITableViewDataSource,UITableVi
             PositionTable_Button.setTitle(Areas[0].TenKV, for: .normal)
         }
         
-
+        
         self.navigationItem.rightBarButtonItem?.customView?.isHidden = isAdded ? false:true
     }
     // MARK: *** UIEvent
-
+    
     @IBAction func Save_Button_Clicked(_ sender: Any, forEvent event: UIEvent) {
         saveToDB()
         self.navigationController?.popViewController(animated: true)
@@ -110,21 +179,21 @@ class TableInfo_ViewController: UIViewController,UITableViewDataSource,UITableVi
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if isAdded2 || Tables[indexSelected_tables].TinhTrang == 0{
-           isAdded = false
+            isAdded = false
             return 0
         }
         else{
-            return Foods.count
+            return list_BookedFoods.count
         }
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! foods_TableViewCell
-
-        cell.nameOfFood_Label.text = Foods[indexPath.row].TenMon
-        cell.food_ImageView.image = UIImage(named: Foods[indexPath.row].Icon)
-        cell.priceOfFood.text = "\(Int(Foods[indexPath.row].Gia!).stringFormattedWithSeparator)đ"
+        
+        cell.nameOfFood_Label.text = list_BookedFoods[indexPath.row].TenMon
+        cell.food_ImageView.image = UIImage(named: list_BookedFoods[indexPath.row].Icon)
+        cell.priceOfFood.text = "\(Int(list_BookedFoods[indexPath.row].Gia!).stringFormattedWithSeparator)đ"
         
         return cell
     }
@@ -134,22 +203,23 @@ class TableInfo_ViewController: UIViewController,UITableViewDataSource,UITableVi
         //performSegue(withIdentifier: "seque_foodDetal", sender: nil)
     }
     
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-       
-        if editingStyle == .delete {
-                // Xoá món trong hoá đơn (Ko phải trong Database)
-            /* 
-                //Xoá trong database
-                let tenmon:String = Foods[indexPath.row].TenMon
-                if edit(query: "DELETE FROM MonAn WHERE MaMon = \(Foods[indexPath.row].MaMon!)"){
-                    Foods.remove(at: indexPath.row)
-                    print("Đã xoá \(tenmon)")
-                    self.foods_TableView.reloadData()
-                }
-            */
-            
-        }
-    }
+    //    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    //
+    //        if editingStyle == .delete {
+    //                // Xoá món trong hoá đơn (Ko phải trong Database)
+    //            /*
+    //                //Xoá trong database
+    //                let tenmon:String = Foods[indexPath.row].TenMon
+    //                if edit(query: "DELETE FROM MonAn WHERE MaMon = \(Foods[indexPath.row].MaMon!)"){
+    //                    Foods.remove(at: indexPath.row)
+    //                    print("Đã xoá \(tenmon)")
+    //                    self.foods_TableView.reloadData()
+    //                }
+    //            */
+    //
+    //        }
+    //    }
+    
     // MARK: *** Function
     
     //Hide keyboard when user touches outside keyboard
@@ -191,16 +261,16 @@ class TableInfo_ViewController: UIViewController,UITableViewDataSource,UITableVi
     }
     
     /*
-    // MARK: *** Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "seque_saveChanged"{
-            //Do anthing....
-        }
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: *** Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     if segue.identifier == "seque_saveChanged"{
+     //Do anthing....
+     }
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
